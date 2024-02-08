9475a0f734fe394db2f8d70f426f360d7ba56de1
horiaomar25 <horiaomar6@gmail.com>
2024-02-08 16:22:59 +0000

    favourites page styling - working on it

--- themealapp/app/favourites/Favoruites.jsx
+++ themealapp/app/favourites/Favoruites.jsx
@@ -1,11 +1,24 @@
+"use client"
 import React from 'react'
 import FavourtiteList from './FavourtiteList'
+import { useState, useEffect } from 'react'
 
 const Favoruites = () => {
+  const [favourites, setFavourites] = useState(
+    JSON.parse(localStorage.getItem("favourites")) || []
+  );
+
+  useEffect(() => {
+  
+    localStorage.setItem("favourites", JSON.stringify(favourites));
+  }, [favourites]);
+
   return (
     <section>
         <h1 className='font-poppins text-4xl font-bold m-4'>Favourites</h1>
-        <FavourtiteList/>
+       
+ 
+        <FavourtiteList favourites={favourites}/>
         
     </section>
   )
--- themealapp/app/favourites/FavourtiteList.jsx
+++ themealapp/app/favourites/FavourtiteList.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+"use client"
+import React from 'react';
 
-const FavourtiteList = () => {
+
+const FavourtiteList = ({favourites}) => {
   return (
-    <div>FavourtiteList</div>
+    <>
+    <h2>Favourtite List</h2>
+    {favourites.map((favourite) => 
+      <div
+        key={favourite.id} className='border-2 border-black w-1/2 m-10 flex flex-col p-6'>    
+      <img src={favourite.strMealThumb} alt={favourite.strMeal}  width={100}/>
+      <h3>{favourite.strMeal}</h3>
+
+      
+      
+      </div>
+  )}
+</>
   )
 }
 
