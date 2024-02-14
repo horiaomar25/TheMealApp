52f8402c72f3a8ffaf8f5d480cd199d611edf4b2
horiaomar25 <horiaomar6@gmail.com>
2024-02-14 16:26:56 +0000

    styling recipe card

--- themealapp/app/Categories/RecipeCard.jsx
+++ themealapp/app/Categories/RecipeCard.jsx
@@ -19,8 +19,6 @@ const RecipeCard = ({ meal, onClose }) => {
     localStorage.setItem("favourites", JSON.stringify(favourites));
   }, [favourites]);
 
- 
-
   // Function to handle toggling favorites
   const handleFavourite = () => {
     if (isFavourite) {
@@ -43,9 +41,8 @@ const RecipeCard = ({ meal, onClose }) => {
     }
   };
 
-
   return (
-    <div className="w-full bg-yellow p-8 relative flex flex-col justify-center items-center">
+    <div className="w-full  bg-yellow p-8 relative flex flex-col justify-center items-center">
       <span
         className="absolute top-0 left-0 m-4 text-3xl cursor-pointer"
         onClick={onClose}
@@ -53,7 +50,10 @@ const RecipeCard = ({ meal, onClose }) => {
         <img src="./back-arrow.png" alt="arrow icon" />
       </span>
 
-      <span className="absolute top-0 right-0 m-4 text-3xl cursor-pointer" onClick={handleFavourite}>
+      <span
+        className="absolute top-0 right-0 m-4 text-3xl cursor-pointer"
+        onClick={handleFavourite}
+      >
         {isFavourite ? (
           <img src="/lover.png" alt="favorite icon" width={30} />
         ) : (
@@ -71,40 +71,31 @@ const RecipeCard = ({ meal, onClose }) => {
       </div>
 
       <div className="w-full p-4 rounded-lg flex flex-col md:flex-row items-center justify-center mt-6">
-        <div className="w-1/2 border-2 border-black rounded-lg p-6 bg-orange mr-6 md:w-1/2">
-          <h2 className="text-xl font-bold mt-2 mb-2 text-center">
+        <div className="w-1/2  rounded-lg p-6  mr-6 md:w-1/2">
+          <h2 className="text-xl font-bold mt-2 mb-4 mr-20 text-center">
             Ingredients:
           </h2>
-          <ul className="space-y-2">
+          <div className="flex flex-wrap justify-between">
             {meal.ingredients.map((ingredient, index) => (
-              <li key={index} className="flex items-center ml-6 text-xl">
-                <input
-                  type="checkbox"
-                  id={`ingredient-${index}`}
-                  checked={checkedIngredients.includes(index)}
-                  onChange={() => toggleIngredient(index)}
-                  className="w-5 h-5 border-2 border-black rounded-sm cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
-                />
-                <label
-                  htmlFor={`ingredient-${index}`}
-                  className={
-                    checkedIngredients.includes(index)
-                      ? "line-through ml-2"
-                      : "ml-2"
-                  }
-                >
-                  {ingredient}
-                </label>
-              </li>
+              <div key={index} className="w-1/2">
+                <ul className="space-y-2">
+                  <li className="flex items-center ml-6 text-lg">
+                    {ingredient}
+                  </li>
+                </ul>
+              </div>
             ))}
-          </ul>
+          </div>
         </div>
         <div className="md:w-1/2">
           <h2 className="text-xl font-bold mt-4 md:mt-2">Instructions:</h2>
-          {/* Map through instructions and create a <p> element for each */}
-          {instructions.map((sentence, index) => (
+          {/* Chunk instructions into groups of 3 and create a <p> element for each group */}
+          {Array.from(
+            { length: Math.ceil(instructions.length / 3) },
+            (v, i) => i
+          ).map((index) => (
             <p key={index} className="mb-2">
-              {sentence.trim()}.
+              {instructions.slice(index * 3, index * 3 + 3).join(". ")}.
             </p>
           ))}
         </div>
--- themealapp/app/favourites/FavourtiteList.jsx
+++ themealapp/app/favourites/FavourtiteList.jsx
@@ -5,17 +5,24 @@ import React from 'react';
 const FavourtiteList = ({favourites}) => {
   return (
     <>
-    <h2>Favourtite List</h2>
+    
+    <section className='border-2 border-black m-4 rounded-xl'>
+      <h2 className='m-6'>Favourtite List</h2>
     {favourites.map((favourite) => 
       <div
-        key={favourite.id} className='border-2 border-black w-1/2 m-10 flex flex-col p-6'>    
-      <img src={favourite.strMealThumb} alt={favourite.strMeal}  width={100}/>
-      <h3>{favourite.strMeal}</h3>
+        key={favourite.id} className='border-2 border-black rounded-xl w-1/5 m-10 flex flex-col p-6'>    
+      <img src={favourite.strMealThumb} alt={favourite.strMeal}  className='w-full'/>
+      <div className='bg-orange w-full'>
+      <h3 className='bg-orange text-xl font-semibold p-4 border border-black'>{favourite.strMeal}</h3>
+      </div>
+      
 
       
       
       </div>
+      
   )}
+  </section>
 </>
   )
 }
