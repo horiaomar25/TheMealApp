fc1fa3b32c6c8b6e68c7f7939f80299b3ffa5977
horiaomar25 <horiaomar6@gmail.com>
2024-04-16 16:54:29 +0100

    update

--- themealapp/app/favourites/Favoruites.jsx
+++ themealapp/app/favourites/Favoruites.jsx
@@ -1,27 +1,31 @@
 "use client"
-import React from 'react'
-import FavourtiteList from './FavourtiteList'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import FavourtiteList from './FavourtiteList';
 
 const Favoruites = () => {
-  const [favourites, setFavourites] = useState(
-    JSON.parse(localStorage.getItem("favourites")) || []
-  );
+  const [favourites, setFavourites] = useState([]);
 
   useEffect(() => {
-  
-    localStorage.setItem("favourites", JSON.stringify(favourites));
+    if (typeof window !== 'undefined') {
+      const storedFavourites = JSON.parse(localStorage.getItem("favourites"));
+      if (storedFavourites) {
+        setFavourites(storedFavourites);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem("favourites", JSON.stringify(favourites));
+    }
   }, [favourites]);
 
   return (
     <section>
-        <h1 className='font-poppins text-4xl font-bold m-4'>Favourites</h1>
-       
- 
-        <FavourtiteList favourites={favourites}/>
-        
+      <h1 className='font-poppins text-4xl font-bold m-4'>Favourites</h1>
+      <FavourtiteList favourites={favourites}/>
     </section>
-  )
+  );
 }
 
-export default Favoruites
+export default Favoruites;
