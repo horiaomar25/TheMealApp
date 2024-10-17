095414c832f2cd6d1ae48af3273ce4bde16d6777
horiaomar25 <horiaomar6@gmail.com>
2024-10-17 15:06:55 +0100

    added: changes to navigation

--- themealapp/app/lib/supabase.jsx
+++
@@ -1,7 +0,0 @@
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
-
-
-export const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-export const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-export const supabase = createClientComponentClient();
